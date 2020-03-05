60d597d6814a67ca2e46c2d01690b015833aa06f
Hristo Yankov <ico.yankov@gmail.com>
2020-03-06 01:24:53 +0200

    Add insertR from chapter3

--- chapter3.rkt
+++ chapter3.rkt
@@ -17,3 +17,54 @@
 
 (equal? (rember 'cup '(coffee cup tea cup and hick cup))
         '(coffee tea cup and hick cup))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l))
+                  (firsts (cdr l)))))))
+
+(equal? (firsts '((apple peach pumpkin)
+                  (plum pear cherry)
+                  (grape raisin pea)
+                  (bean carrot eggplant)))
+        '(apple plum grape bean))
+
+(equal? (firsts '((a b)
+                  (c d)
+                  (e f)))
+        '(a c e))
+
+(equal? (firsts '())
+        '())
+
+(equal? (firsts '((five plums)
+                  (four)
+                  (eleven green oranges)))
+        '(five four eleven))
+
+(equal? (firsts '(((five plums) four)
+                  (eleven green oranges)
+                  ((no) more)))
+        '((five plums) eleven (no)))
+
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons old
+                                 (cons new (cdr lat))))
+      (else (cons (car lat) (insertR new old (cdr lat)))))))
+
+(equal? (insertR 'topping 'fudge '(ice cream with fudge for desert))
+        '(ice cream with fudge topping for desert))
+
+(equal? (insertR 'jalapeno 'and '(tacos tamales and salsa))
+        '(tacos tamales and jalapeno salsa))
+
+(equal? (insertR 'e 'd '(a b c d f g d h))
+        '(a b c d e f g d h))
+
+
